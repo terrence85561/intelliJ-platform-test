5948a37152db1629f806f1d7e76e512f4e1023ba
GitHub Action <action@github.com>
2025-02-25 02:36:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/terrence85561/intellijplatformtest/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/terrence85561/intellijplatformtest/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.terrence85561.intellijplatformtest.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/terrence85561/intellijplatformtest/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/terrence85561/intellijplatformtest/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.terrence85561.intellijplatformtest.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.terrence85561.intellijplatformtest.MyBundle
+import com.github.terrence85561.intellijplatformtest.services.MyProjectService
 import javax.swing.JButton
 
 
